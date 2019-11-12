fd268234845ecbe44e707df2fe3e679c24e4e628
Sandeep <sunthapa1@hotmail.com>
2019-11-12 20:35:25 +0000

    Started writing functional ladder for object detection with yolo

---
+++ cv_homework.py
@@ -0,0 +1,96 @@
+#####################################################################
+
+import cv2
+import os
+import numpy as np
+import random
+import csv
+
+master_path_to_dataset = "/Users/sandeep/Desktop/Homework2019/ComputerVision Homework/TTBB-durham-02-10-17-sub10" # ** need to edit this **
+directory_to_cycle_left = "left-images"     # edit this if needed
+directory_to_cycle_right = "right-images"   # edit this if needed
+
+#####################################################################
+# fixed camera parameters for this stereo setup (from calibration)
+
+camera_focal_length_px = 399.9745178222656  # focal length in pixels
+camera_focal_length_m = 4.8 / 1000          # focal length in metres (4.8 mm)
+stereo_camera_baseline_m = 0.2090607502     # camera baseline in metres
+
+image_centre_h = 262.0;
+image_centre_w = 474.5;
+
+#####################################################################
+
+
+## project_disparity_to_3d : project a given disparity image
+## (uncropped, unscaled) to a set of 3D points with optional colour
+
+def project_disparity_to_3d(disparity, max_disparity, rgb=[]):
+
+    points = [];
+
+    f = camera_focal_length_px;
+    B = stereo_camera_baseline_m;
+
+    height, width = disparity.shape[:2];
+
+    # assume a minimal disparity of 2 pixels is possible to get Zmax
+    # and then we get reasonable scaling in X and Y output if we change
+    # Z to Zmax in the lines X = ....; Y = ...; below
+
+    # Zmax = ((f * B) / 2);
+
+    for y in range(height): # 0 - height is the y axis index
+        for x in range(width): # 0 - width is the x axis index
+
+            # if we have a valid non-zero disparity
+
+            if (disparity[y,x] > 0):
+
+                # calculate corresponding 3D point [X, Y, Z]
+
+                # stereo lecture - slide 22 + 25
+
+                Z = (f * B) / disparity[y,x];
+
+                X = ((x - image_centre_w) * Z) / f;
+                Y = ((y - image_centre_h) * Z) / f;
+
+                # add to points
+
+                if(rgb.size > 0):
+                    points.append([X,Y,Z,rgb[y,x,2], rgb[y,x,1],rgb[y,x,0]]);
+                else:
+                    points.append([X,Y,Z]);
+
+    return points;
+
+#####################################################################
+# project a set of 3D points back the 2D image domain
+
+def project_3D_points_to_2D_image_points(points):
+
+    points2 = [];
+
+    # calc. Zmax as per above
+
+    # Zmax = (camera_focal_length_px * stereo_camera_baseline_m) / 2;
+
+    for i1 in range(len(points)):
+
+        # reverse earlier projection for X and Y to get x and y again
+
+        x = ((points[i1][0] * camera_focal_length_px) / points[i1][2]) + image_centre_w;
+        y = ((points[i1][1] * camera_focal_length_px) / points[i1][2]) + image_centre_h;
+        points2.append([x,y]);
+
+    return points2;
+
+#####################################################################
+#Yolo for object detection
+# - Pass in imageL in to yolo
+#     -How to pass images files into yolo?
+# - Get detection calss and bounding box location interms of pixel coordinates 
+#     - How to do dectection in yolo?
+
